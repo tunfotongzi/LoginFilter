fede5f128af79f249acbf8f9abcdbc0a77494956
tunfotongzi <929090967@qq.com>
2017-06-09 17:30:25 +0800

    过滤器过滤项目所有页面，要求每个页面都需要登录后才能访问(除了login.jsp页面本身)

---
+++ src/com/imooc/filter/LoginFilter.java
@@ -0,0 +1,61 @@
+package com.imooc.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName="loginFilter", urlPatterns="/*")
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+		
+		// 先将ServletRequest转为它带有HTTP操作方法的子类
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		
+		//获取登录信息
+		HttpSession session = request.getSession();
+		
+		//记录未登录前的所有可能的地址
+//		String noLoginPaths = config.getInitParameter("noLoginPaths");
+		
+		//因为该过滤器是针对所有页面设置，所以可以用于设置字符集，而不需要每个Servlet中都去设置
+		request.setCharacterEncoding("UTF-8");
+		
+		//如果页面是login.jsp那么直接放行并return，不再继续向下执行代码
+		if(request.getRequestURI().indexOf("login.jsp")!=-1){
+			arg2.doFilter(arg0, arg1);
+			return;
+		}
+		
+		
+		// 如果用户已经登录就继续请求web资源，否则跳转登录页面
+		if(session.getAttribute("username")!=null){
+			arg2.doFilter(arg0, arg1);
+		}else{
+			response.sendRedirect("login.jsp");
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
---
+++ src/com/imooc/servlet/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.imooc.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		System.out.println(username);
+		
+		if("admin".equals(username) && "admin".equals(password)){
+			//记住登录状态
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+			response.sendRedirect(request.getContextPath()+"/success.jsp");
+		}else{
+			//登录失败条状会失败页面
+			response.sendRedirect(request.getContextPath()+"/fail.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
